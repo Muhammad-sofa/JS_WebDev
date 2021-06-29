4bf88b1a376e40186708ec2348d5e436a50a4097
Muhammad Sofa Yuliansyah <muhammadsofa52@yahoo.com>
2021-06-29 12:55:35 +0700

    Add all my Files

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import modulName from 'elements/button'
+
+export default class LandingPage extends Component {
+     render() {
+          return (
+               <div>
+                    
+               </div>
+          )
+     }
+}
