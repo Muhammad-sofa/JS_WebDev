d7f7e9953b933cbc5f0f9655d13cbc86f852df8c
Muhammad Sofa Yuliansyah <muhammadsofa52@yahoo.com>
2021-06-30 00:11:13 +0700

    Update Variable Color

---
+++ src/pages/IconText.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Button from 'elements/button'
+
+export default function IconText() {
+     return (
+          <Button className="brand-text-con" href="" type="link">
+               Stay <span className="text-gray-900">Travelling</span>
+          </Button>
+     )
+}
