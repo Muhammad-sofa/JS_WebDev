715e8bebdb3b973d3e717ce8a5b7835c300dbe5a
Muhammad Sofa Yuliansyah <muhammadsofa52@yahoo.com>
2021-07-19 01:35:11 +0700

    Component Hero

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 
-import Header from 'parts/Header'
+import Header from 'parts/Header';
+import Hero from 'parts/Hero';
+
+import landingPage from 'json/landingPage';
 
 export default class LandingPage extends Component {
      render() {
           return (
                <>
-                   <Header {...this.props}></Header> 
+               <Header {...this.props}></Header>
+               <hero data={landingPage.hero} /> 
                </>
           )
      }
