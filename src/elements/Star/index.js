6fc6272f28870d696270e085a92e44394712bbee
Muhammad Sofa Yuliansyah <muhammadsofa52@yahoo.com>
2021-08-20 23:08:24 +0700

    Testing Component

--- src/elements/Star/index.js
+++ src/elements/Star/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import propTypes from "prop-types";
 
-import "./index";
+import "./index.scss";
 
 export default function Star({ className, value, height, width, spacing }) {
   const decimals = Number(value) % 1;
